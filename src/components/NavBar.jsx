e055a4c8f9ff4e1dcd1abd27d00eca943554659d
blakee-marcus <marcusb733@gmail.com>
2025-05-26 23:42:55 -0700

    fix: increase nav bar size

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -20,33 +20,62 @@ function NavBar() {
     <header className='flex flex-col md:flex-row md:items-center justify-between px-6 py-4 bg-sinner text-saint shadow-lg gap-y-2'>
       <h1 className='text-2xl font-serif tracking-wider'>Saints & Sinners</h1>
 
-      <nav className='flex flex-wrap items-center gap-x-4 text-sm relative'>
-        <Link to='/' className='hover:text-goldleaf transition'>Home</Link>
-        <Link to='/menu' className='hover:text-goldleaf transition'>Menu</Link>
-        <Link to='/story' className='hover:text-goldleaf transition'>Our Story</Link>
+      <nav className='flex flex-col md:flex-row md:items-center gap-y-2 md:gap-x-4 text-base md:text-sm relative'>
+        <Link to='/' className='hover:text-goldleaf transition'>
+          Home
+        </Link>
+        <Link to='/menu' className='hover:text-goldleaf transition'>
+          Menu
+        </Link>
+        <Link to='/story' className='hover:text-goldleaf transition'>
+          Our Story
+        </Link>
 
         <div className='relative' ref={dropdownRef}>
           <button
-            onClick={() => setCustomOpen(prev => !prev)}
-            className='hover:text-goldleaf transition inline-flex items-center'
-          >
+            onClick={() => setCustomOpen((prev) => !prev)}
+            className='hover:text-goldleaf transition inline-flex items-center'>
             Custom Orders <ChevronDown size={16} className='ml-1' />
           </button>
 
           {customOpen && (
             <div className='absolute left-0 mt-2 bg-sinner border border-goldleaf rounded shadow-lg z-50 w-56'>
-              <Link to='/specialty-cakes' className='block px-4 py-2 hover:bg-goldleaf hover:text-sinner transition'>Specialty Cakes</Link>
-              <Link to='/custom-cakes' className='block px-4 py-2 hover:bg-goldleaf hover:text-sinner transition'>Custom Cakes</Link>
-              <Link to='/custom-cookies' className='block px-4 py-2 hover:bg-goldleaf hover:text-sinner transition'>Custom Cookies</Link>
-              <Link to='/cookie-cakes' className='block px-4 py-2 hover:bg-goldleaf hover:text-sinner transition'>Cookie Cakes</Link>
-              <Link to='/custom-cupcakes' className='block px-4 py-2 hover:bg-goldleaf hover:text-sinner transition'>Custom Cupcakes</Link>
-              <Link to='/cake-bombs' className='block px-4 py-2 hover:bg-goldleaf hover:text-sinner transition'>Cake Bombs</Link>
+              <Link
+                to='/specialty-cakes'
+                className='block px-4 py-2 hover:bg-goldleaf hover:text-sinner transition'>
+                Specialty Cakes
+              </Link>
+              <Link
+                to='/custom-cakes'
+                className='block px-4 py-2 hover:bg-goldleaf hover:text-sinner transition'>
+                Custom Cakes
+              </Link>
+              <Link
+                to='/custom-cookies'
+                className='block px-4 py-2 hover:bg-goldleaf hover:text-sinner transition'>
+                Custom Cookies
+              </Link>
+              <Link
+                to='/cookie-cakes'
+                className='block px-4 py-2 hover:bg-goldleaf hover:text-sinner transition'>
+                Cookie Cakes
+              </Link>
+              <Link
+                to='/custom-cupcakes'
+                className='block px-4 py-2 hover:bg-goldleaf hover:text-sinner transition'>
+                Custom Cupcakes
+              </Link>
+              <Link
+                to='/cake-bombs'
+                className='block px-4 py-2 hover:bg-goldleaf hover:text-sinner transition'>
+                Cake Bombs
+              </Link>
             </div>
           )}
         </div>
-
-        <Link to='/toasts' className='hover:text-goldleaf transition'>Toasts</Link>
-        <Link to='/contact' className='hover:text-goldleaf transition'>Contact</Link>
+        <Link to='/contact' className='hover:text-goldleaf transition'>
+          Contact
+        </Link>
       </nav>
     </header>
   );
